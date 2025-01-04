70e7f2a0ab7baaebd09ddd642fa8424aba0af979
rheyhannh <rheyhannh@student.ub.ac.id>
2025-01-04 20:33:09 +0700

    feat: Add schema

---
+++ src/schemas/envVariables.js
@@ -0,0 +1,16 @@
+/** 
+ * Environment variables schema that used for validation
+ */
+const schema = /** @type {import('@fastify/env').FastifyEnvOptions['schema']} */ ({
+    type: 'object',
+    required: [
+        'ILOVEAPI_PUBLIC_KEY',
+        'ILOVEAPI_SECRET_KEY',
+    ],
+    properties: {
+        ILOVEAPI_PUBLIC_KEY: { type: 'string' },
+        ILOVEAPI_SECRET_KEY: { type: 'string' }
+    }
+})
+
+export default schema;
