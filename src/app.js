330635d7eb7fdaccd9e6d18d64f856f217f83b36
rheyhannh <rheyhannh@student.ub.ac.id>
2025-01-05 18:53:00 +0700

    style: Prettier code

--- src/app.js
+++ src/app.js
@@ -8,31 +8,31 @@ import apiRoute from './routes/apiRoute.js';
 
 const { __dirname } = getFilenameAndDirname(import.meta.url);
 
-const fastify = /** @type {import('fastify').FastifyInstance} */ (Fastify({
-    logger: {
-        transport: {
-            target: 'pino-pretty',
-            options: {
-                translateTime: 'HH:MM:ss Z',
-                ignore: 'pid,hostname',
-            },
-        },
-    },
-}))
+const fastify = /** @type {import('fastify').FastifyInstance} */ (
+	Fastify({
+		logger: {
+			transport: {
+				target: 'pino-pretty',
+				options: {
+					translateTime: 'HH:MM:ss Z',
+					ignore: 'pid,hostname'
+				}
+			}
+		}
+	})
+);
 
-fastify
-    .register(import('@fastify/env'), {
-        confKey: 'config',
-        schema: envVariablesSchema,
-        data: process.env,
-        dotenv: true
-    });
+fastify.register(import('@fastify/env'), {
+	confKey: 'config',
+	schema: envVariablesSchema,
+	data: process.env,
+	dotenv: true
+});
 
-fastify
-    .register(import('@fastify/static'), {
-        root: path.join(__dirname, 'public'),
-        prefix: '/public/',
-    });
+fastify.register(import('@fastify/static'), {
+	root: path.join(__dirname, 'public'),
+	prefix: '/public/'
+});
 
 fastify.register(apiRoute);
 
