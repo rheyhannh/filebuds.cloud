9fff4e38ff81388850efcfeb867228008a0fa7fb
Reyhan Naufal Hayyan <65756168+rheyhannh@users.noreply.github.com>
2025-01-27 19:25:21 +0700

    chore: Fix eslint configuration for mocha

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import mochaPlugin from 'eslint-plugin-mocha';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
 	{
 		languageOptions: {
-			globals: globals.node
+			globals: {
+				...globals.node,
+				...globals.mocha
+			}
 		}
 	},
-	pluginJs.configs.recommended,
-	mochaPlugin.configs.recommended
+	pluginJs.configs.recommended
 ];
