a21f2fdb32cb66b6283204ff36416afe353c865e
Reyhan Naufal Hayyan <65756168+rheyhannh@users.noreply.github.com>
2025-02-14 01:38:52 +0700

    test: Add test case for error module

--- test/lib/iloveimg/Error.spec.js
+++ test/lib/iloveimg/Error.spec.js
@@ -8,32 +8,108 @@ import {
 
 describe('ILoveIMGApi Error.classifyError() Tests', function () {
 	it('should throw ILoveApiError with response message', function () {
-		const error = {
-			isAxiosError: true,
-			response: {
-				status: 404,
-				data: { message: 'Not Found', code: 1001 }
+		const error = [
+			{
+				isAxiosError: true,
+				response: {
+					status: 404,
+					data: { message: 'Not Found', code: 1001 }
+				}
+			},
+			{
+				isAxiosError: true,
+				response: {
+					status: 404,
+					data: { message: 'Not Found', code: '' }
+				}
+			},
+			{
+				isAxiosError: true,
+				response: {
+					status: 404,
+					data: { message: 'Not Found', code: null }
+				}
+			},
+			{
+				isAxiosError: true,
+				response: {
+					status: 404,
+					data: { message: 'Not Found', code: undefined }
+				}
 			}
-		};
+		];
 
-		expect(() => classifyError(error)).to.throw(
+		expect(() => classifyError(error[0])).to.throw(
 			ILoveApiError,
 			'Not Found (Status: 404, Code: 1001)'
+		);
+
+		expect(() => classifyError(error[1])).to.throw(
+			ILoveApiError,
+			'Not Found (Status: 404, Code: -1)'
+		);
+
+		expect(() => classifyError(error[2])).to.throw(
+			ILoveApiError,
+			'Not Found (Status: 404, Code: -1)'
+		);
+
+		expect(() => classifyError(error[3])).to.throw(
+			ILoveApiError,
+			'Not Found (Status: 404, Code: -1)'
 		);
 	});
 
 	it('should throw ILoveApiError with error.message inside response.error', function () {
-		const error = {
-			isAxiosError: true,
-			response: {
-				status: 500,
-				data: { error: { message: 'Internal Server Error', code: 2001 } }
+		const error = [
+			{
+				isAxiosError: true,
+				response: {
+					status: 500,
+					data: { error: { message: 'Internal Server Error', code: 2001 } }
+				}
+			},
+			{
+				isAxiosError: true,
+				response: {
+					status: 500,
+					data: { error: { message: 'Internal Server Error', code: '' } }
+				}
+			},
+			{
+				isAxiosError: true,
+				response: {
+					status: 500,
+					data: { error: { message: 'Internal Server Error', code: null } }
+				}
+			},
+			{
+				isAxiosError: true,
+				response: {
+					status: 500,
+					data: { error: { message: 'Internal Server Error', code: undefined } }
+				}
 			}
-		};
+		];
 
-		expect(() => classifyError(error)).to.throw(
+		expect(() => classifyError(error[0])).to.throw(
 			ILoveApiError,
 			'Internal Server Error (Status: 500, Code: 2001)'
+		);
+
+		expect(() => classifyError(error[1])).to.throw(
+			ILoveApiError,
+			'Internal Server Error (Status: 500, Code: -1)'
+		);
+
+		expect(() => classifyError(error[2])).to.throw(
+			ILoveApiError,
+			'Internal Server Error (Status: 500, Code: -1)'
+		);
+
+		expect(() => classifyError(error[3])).to.throw(
+			ILoveApiError,
+			'Internal Server Error (Status: 500, Code: -1)'
 		);
 	});
 
