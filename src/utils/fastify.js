c20a8025468fd11408a1bff48862d0f2f929c233
Reyhan Naufal Hayyan <65756168+rheyhannh@users.noreply.github.com>
2025-03-08 12:13:49 +0700

    fix: Fastify utils

--- src/utils/fastify.js
+++ src/utils/fastify.js
@@ -30,23 +30,30 @@ export const getFilenameAndDirname = (metaUrl) => {
 /**
  * Middleware to validate API requests based on API key or origin domain.
  * This function verifies if the request is authorized by checking:
- * - If the API key provided in the request matches the expected key.
+ * - If the API key provided in `apikey` header or query param matches the expected key.
  * - If the request's origin or referer is from an allowed domain.
  *
- * @param {import('fastify').FastifyRequest} request - The incoming request object from Fastify.
- * @param {Array<string>} allowedDomains - List of allowed domains for origin validation.
+ * When one of these condition meet, request is allowed to proceed.
+ * @param {import('fastify').FastifyRequest} request The incoming request object from Fastify.
+ * @param {Array<string>} allowedDomains List of allowed domains for origin validation.
  * @returns {boolean} `true` if the request is valid, otherwise `false`.
  */
 export const isValidRequest = (request, allowedDomains) => {
-	// Retrieve API key from headers or params
-	const apiKey =
-		request.headers?.['x-filebuds-apikey'] || request.params?.['apikey'];
+	// Retrieve API key from headers or query params.
+	const apiKey = request.headers?.apikey || request.query?.apikey;
 
-	// Validate API key
+	// Validate API key.
 	if (apiKey === APP_SECRET_KEY) return true;
 
-	// Retrieve origin from headers
-	const origin = request.headers.origin || request.headers.referer;
+	// Reject requests when 'allowedDomains' is empty array or not an array.
+	if (!Array.isArray(allowedDomains) || !allowedDomains.length) {
+		return false;
+	}
+
+	// Retrieve origin from headers.
+	const origin = request.headers?.origin || request.headers?.referer;
+
+	// Reject requests when origin or referer not provided.
 	if (!origin) return false;
 
 	try {
