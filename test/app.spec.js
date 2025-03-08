11455590068cad5b7d129755845a4b98de2cc8db
Reyhan Naufal Hayyan <65756168+rheyhannh@users.noreply.github.com>
2025-03-08 15:40:37 +0700

    fix: Test suite use fastify inject instead chai http

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,23 +1,17 @@
 import { describe, it } from 'mocha';
-import { expect, use } from 'chai';
-import chaiHttp from 'chai-http';
+import { expect } from 'chai';
 import config from '../src/config/global.js';
 import buildFastify from '../src/app.js';
 
 const { APP_SECRET_KEY } = config;
-const chai = use(chaiHttp);
 
 describe('[Integration] ILoveAPI Webhook', () => {
 	const endpoint = '/iloveapi';
 	let fastify = /** @type {ReturnType<typeof buildFastify>} */ (undefined);
-	let server = /** @type {ReturnType<typeof buildFastify>['server']} */ (
-		undefined
-	);
 
-	before(function () {
+	before(async function () {
 		fastify = buildFastify();
-		fastify.ready();
-		server = fastify.server;
+		await fastify.ready();
 	});
 
 	after(async function () {
@@ -25,7 +19,7 @@ describe('[Integration] ILoveAPI Webhook', () => {
 	});
 
 	describe('POST /iloveapi', () => {
-		it('should return 400 response error when requests body is invalid', function (done) {
+		it('should return 400 response error when requests body is invalid', async function () {
 			const payloads =
 				/** @type {Array<import('../src/schemas/iloveapi.js').CallbackRequestBodyProps>} */ ([
 					{ event: 'task.unknown' },
@@ -168,38 +162,150 @@ describe('[Integration] ILoveAPI Webhook', () => {
 					}
 				]);
 
-			Promise.all(
-				payloads.map((payload) => {
-					return new Promise((resolve, reject) => {
-						chai.request
-							.execute(server)
-							.post(endpoint)
-							.send(payload)
-							.end((err, res) => {
-								if (err) return reject(err);
-								try {
-									expect(res).to.have.status(400);
-									expect(res.body).to.deep.include({
-										ok: false,
-										statusCode: 400,
-										statusText: 'Bad Request'
-									});
-									expect(res.body.error)
-										.to.be.an('object')
-										.that.includes.all.keys('name', 'message');
-									resolve();
-								} catch (assertionError) {
-									reject(assertionError);
-								}
-							});
-					});
+			for (const payload of payloads) {
+				const response = await fastify.inject({
+					method: 'POST',
+					url: endpoint,
+					body: payload
+				});
+
+				expect(response.statusCode).to.equal(400);
+				expect(response.json()).to.deep.include({
+					ok: false,
+					statusCode: 400,
+					statusText: 'Bad Request'
+				});
+				expect(response.json().error)
+					.to.be.an('object')
+					.that.includes.all.keys('name', 'message');
+			}
+		});
+
+		it('should return 401 response error when requests are unauthorized', async function () {
+			const payload =
+				/** @type {import('../src/schemas/iloveapi.js').CallbackRequestBodyProps} */ ({
+					event: 'task.completed',
+					data: {
+						task: {
+							tool: 'compress',
+							process_start: '2024-06-17 10:18:17',
+							custom_int: null,
+							custom_string: null,
+							status: 'TaskSuccess',
+							status_message: 'This task has been processed successfully.',
+							timer: '0.206',
+							filesize: 13264,
+							output_filesize: 9792,
+							output_filenumber: 1,
+							output_extensions: ['pdf'],
+							server: 'api70.ilovepdf.com',
+							task: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+							file_number: '1',
+							download_filename: 'server.pdf'
+						}
+					}
+				});
+
+			const response = await fastify.inject({
+				method: 'POST',
+				url: endpoint,
+				body: payload
+			});
+
+			expect(response.statusCode).to.equal(401);
+			expect(response.json()).to.deep.include({
+				ok: false,
+				statusCode: 401,
+				statusText: 'Unauthorized'
+			});
+			expect(response.json().error)
+				.to.be.an('object')
+				.that.includes.all.keys('name', 'message');
+		});
+
+		it("should return 200 response when requests body valid and authorized with 'apikey' header", async function () {
+			const payload =
+				/** @type {import('../src/schemas/iloveapi.js').CallbackRequestBodyProps} */ ({
+					event: 'task.completed',
+					data: {
+						task: {
+							tool: 'compress',
+							process_start: '2024-06-17 10:18:17',
+							custom_int: null,
+							custom_string: null,
+							status: 'TaskSuccess',
+							status_message: 'This task has been processed successfully.',
+							timer: '0.206',
+							filesize: 13264,
+							output_filesize: 9792,
+							output_filenumber: 1,
+							output_extensions: ['pdf'],
+							server: 'api70.ilovepdf.com',
+							task: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+							file_number: '1',
+							download_filename: 'server.pdf'
+						}
+					}
+				});
+
+			const response = await fastify.inject({
+				method: 'POST',
+				url: endpoint,
+				headers: { apikey: APP_SECRET_KEY },
+				body: payload
+			});
+
+			expect(response.statusCode).to.equal(200);
+			expect(response.json()).to.deep.include({
+				ok: true,
+				statusCode: 200,
+				statusText: 'OK'
+			});
+			expect(response.json().data).to.be.an('object');
+		});
+
+		it("should return 200 response when requests body valid and authorized with 'apikey' query param", async function () {
+			const payload =
+				/** @type {import('../src/schemas/iloveapi.js').CallbackRequestBodyProps} */ ({
+					event: 'task.completed',
+					data: {
+						task: {
+							tool: 'compress',
+							process_start: '2024-06-17 10:18:17',
+							custom_int: null,
+							custom_string: null,
+							status: 'TaskSuccess',
+							status_message: 'This task has been processed successfully.',
+							timer: '0.206',
+							filesize: 13264,
+							output_filesize: 9792,
+							output_filenumber: 1,
+							output_extensions: ['pdf'],
+							server: 'api70.ilovepdf.com',
+							task: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
+							file_number: '1',
+							download_filename: 'server.pdf'
+						}
+					}
+				});
+
+			const response = await fastify
+				.inject({
+					query: { apikey: APP_SECRET_KEY },
+					body: payload
 				})
-			)
-				.then(() => done())
-				.catch(done);
+				.post(endpoint);
+
+			expect(response.statusCode).to.equal(200);
+			expect(response.json()).to.deep.include({
+				ok: true,
+				statusCode: 200,
+				statusText: 'OK'
+			});
+			expect(response.json().data).to.be.an('object');
 		});
 
-		it('should return 401 response error when requests are unauthorized', function (done) {
+		it("should return 200 response when requests body valid and authorized with 'apikey' header and query param", async function () {
 			const payload =
 				/** @type {import('../src/schemas/iloveapi.js').CallbackRequestBodyProps} */ ({
 					event: 'task.completed',
@@ -224,149 +330,21 @@ describe('[Integration] ILoveAPI Webhook', () => {
 					}
 				});
 
-			chai.request
-				.execute(server)
-				.post(endpoint)
-				.send(payload)
-				.end((err, res) => {
-					expect(res).to.have.status(401);
-					expect(res.body).to.deep.include({
-						ok: false,
-						statusCode: 401,
-						statusText: 'Unauthorized'
-					});
-					expect(res.body.error)
-						.to.be.an('object')
-						.that.includes.all.keys('name', 'message');
-					done(err);
-				});
-		});
+			const response = await fastify
+				.inject({
+					headers: { apikey: APP_SECRET_KEY },
+					query: { apikey: APP_SECRET_KEY },
+					body: payload
+				})
+				.post(endpoint);
 
-		it("should return 200 response when requests body valid and authorized with 'apikey' header", function (done) {
-			const payload =
-				/** @type {import('../src/schemas/iloveapi.js').CallbackRequestBodyProps} */ ({
-					event: 'task.completed',
-					data: {
-						task: {
-							tool: 'compress',
-							process_start: '2024-06-17 10:18:17',
-							custom_int: null,
-							custom_string: null,
-							status: 'TaskSuccess',
-							status_message: 'This task has been processed successfully.',
-							timer: '0.206',
-							filesize: 13264,
-							output_filesize: 9792,
-							output_filenumber: 1,
-							output_extensions: ['pdf'],
-							server: 'api70.ilovepdf.com',
-							task: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
-							file_number: '1',
-							download_filename: 'server.pdf'
-						}
-					}
-				});
-
-			chai.request
-				.execute(server)
-				.post(endpoint)
-				.set('apikey', APP_SECRET_KEY)
-				.send(payload)
-				.end((err, res) => {
-					expect(res).to.have.status(200);
-					expect(res.body).to.deep.include({
-						ok: true,
-						statusCode: 200,
-						statusText: 'OK'
-					});
-					expect(res.body.data).to.be.an('object');
-					done(err);
-				});
-		});
-
-		it("should return 200 response when requests body valid and authorized with 'apikey' query param", function (done) {
-			const payload =
-				/** @type {import('../src/schemas/iloveapi.js').CallbackRequestBodyProps} */ ({
-					event: 'task.completed',
-					data: {
-						task: {
-							tool: 'compress',
-							process_start: '2024-06-17 10:18:17',
-							custom_int: null,
-							custom_string: null,
-							status: 'TaskSuccess',
-							status_message: 'This task has been processed successfully.',
-							timer: '0.206',
-							filesize: 13264,
-							output_filesize: 9792,
-							output_filenumber: 1,
-							output_extensions: ['pdf'],
-							server: 'api70.ilovepdf.com',
-							task: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
-							file_number: '1',
-							download_filename: 'server.pdf'
-						}
-					}
-				});
-
-			chai.request
-				.execute(server)
-				.post(endpoint)
-				.query({ apikey: APP_SECRET_KEY })
-				.send(payload)
-				.end((err, res) => {
-					expect(res).to.have.status(200);
-					expect(res.body).to.deep.include({
-						ok: true,
-						statusCode: 200,
-						statusText: 'OK'
-					});
-					expect(res.body.data).to.be.an('object');
-					done(err);
-				});
-		});
-
-		it("should return 200 response when requests body valid and authorized with 'apikey' header and query param", function (done) {
-			const payload =
-				/** @type {import('../src/schemas/iloveapi.js').CallbackRequestBodyProps} */ ({
-					event: 'task.completed',
-					data: {
-						task: {
-							tool: 'compress',
-							process_start: '2024-06-17 10:18:17',
-							custom_int: null,
-							custom_string: null,
-							status: 'TaskSuccess',
-							status_message: 'This task has been processed successfully.',
-							timer: '0.206',
-							filesize: 13264,
-							output_filesize: 9792,
-							output_filenumber: 1,
-							output_extensions: ['pdf'],
-							server: 'api70.ilovepdf.com',
-							task: 'xxxxxxxxxxxxxxxxxxxxxxxxx',
-							file_number: '1',
-							download_filename: 'server.pdf'
-						}
-					}
-				});
-
-			chai.request
-				.execute(server)
-				.post(endpoint)
-				.set('apikey', APP_SECRET_KEY)
-				.query({ apikey: APP_SECRET_KEY })
-				.send(payload)
-				.end((err, res) => {
-					expect(res).to.have.status(200);
-					expect(res.body).to.deep.include({
-						ok: true,
-						statusCode: 200,
-						statusText: 'OK'
-					});
-					expect(res.body.data).to.be.an('object');
-					done(err);
-				});
+			expect(response.statusCode).to.equal(200);
+			expect(response.json()).to.deep.include({
+				ok: true,
+				statusCode: 200,
+				statusText: 'OK'
+			});
+			expect(response.json().data).to.be.an('object');
 		});
 	});
 });
