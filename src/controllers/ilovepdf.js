27d4b24fc85cb5b88aa0d0e14213f4366c1ba405
Reyhan Naufal Hayyan <65756168+rheyhannh@users.noreply.github.com>
2025-04-18 13:15:39 +0700

    feat: Add PDF merge controller

--- src/controllers/ilovepdf.js
+++ src/controllers/ilovepdf.js
@@ -47,3 +47,54 @@ export const imageToPdf = async (jobId, userId, imageUrl) => {
 	// Call the service function to convert image file to PDF.
 	return await Service.imageToPdf(jobId, userId, imageUrl, fileDetails);
 };
+
+/**
+ * Processes an PDF merge and returns the operation status.
+ * This function performs the following steps:
+ * 1. Extracts the original file details (name, extension) from the given files URL.
+ * 2. Generates `fileDetails` object to determine original and output files information.
+ * 3. Calls the `Service.mergePdf` method to merge PDF files.
+ *
+ * If any step fails, it throws an `Error` with corresponding error message.
+ * Otherwise, it returns object indicating operation are success.
+ *
+ * @param {string} jobId Job identifier.
+ * @param {number} userId Unique identifier of the user requesting PDF merge.
+ * @param {Array<string>} filesUrl Array of public URL of PDF files to process.
+ * @throws {Error} If any step fails.
+ * @returns {Promise<ILoveApiTypes.TaskCreationResult>} Resolve an object indicating operation are success.
+ */
+export const mergePdf = async (jobId, userId, filesUrl) => {
+	if (
+		typeof jobId !== 'string' ||
+		typeof userId !== 'number' ||
+		!Array.isArray(filesUrl) ||
+		filesUrl.length < 2
+	)
+		throw new Error('Missing required parameters.');
+
+	// Extract original file information from the provided file URL.
+	const originalFileDetails = filesUrl.map((fileUrl) =>
+		Util.getOriginalFileInformationFromURL(fileUrl)
+	);
+	if (
+		!originalFileDetails ||
+		!Array.isArray(originalFileDetails) ||
+		originalFileDetails.some((x) => !x)
+	) {
+		throw new Error('Failed to resolve original file details.');
+	}
+
+	const fileDetails =
+		/** @type {{original:Array<ILoveApiTypes.FileInformationProps['original']>, output:ILoveApiTypes.FileInformationProps['output']}} */ ({
+			original: originalFileDetails,
+			output: {
+				name: jobId,
+				extension: 'pdf',
+				filename: jobId + '.pdf'
+			}
+		});
+
+	// Call the service function to merge PDFs.
+	return await Service.mergePdf(jobId, userId, filesUrl, fileDetails);
+};
--- test/controllers/ilovepdf.spec.js
+++ test/controllers/ilovepdf.spec.js
@@ -94,4 +94,162 @@ describe('[Integration] ILovePDF Controllers', () => {
 			expect(result).to.be.deep.equal(mockServiceValue);
 		});
 	});
+
+	describe('mergePdf()', () => {
+		let spyServiceStub;
+		const mockServiceValue = {
+			server: null,
+			task_id: 'some_task_id_from_iloveapi_servers',
+			files: [
+				{
+					server_filename: 'lorems.pdf',
+					filename: 'lorem.pdf'
+				},
+				{
+					server_filename: 'ipsums.pdf',
+					filename: 'ipsum.pdf'
+				},
+				{
+					server_filename: 'dolors.pdf',
+					filename: 'dolor.pdf'
+				}
+			]
+		};
+
+		before(() => {
+			spyServiceStub = sinon
+				.stub(ILovePDFService.default, 'mergePdf')
+				.resolves(mockServiceValue);
+		});
+
+		after(() => {
+			sinon.restore();
+		});
+
+		it('should resolves the mocked value from the service', async () => {
+			const result = await ILovePDFService.default.mergePdf();
+			expect(result).to.be.deep.equal(mockServiceValue);
+		});
+
+		it('should rejects with an Error when required parameters missing', async () => {
+			await expect(
+				ILovePDFController.mergePdf(null, 185150, [
+					'https://api.telegram.org/others/lorem.pdf',
+					'https://api.telegram.org/others/sit.pdf'
+				])
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', null, [
+					'https://api.telegram.org/others/lorem.pdf',
+					'https://api.telegram.org/others/sit.pdf'
+				])
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, null)
+			).to.be.rejectedWith('Missing required parameters.');
+		});
+
+		it('should rejects with an Error when filesUrl are not an Array or length are less than 2', async () => {
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, undefined)
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, true)
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, false)
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, {})
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, 999)
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, 'lorems')
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, [])
+			).to.be.rejectedWith('Missing required parameters.');
+
+			await expect(
+				ILovePDFController.mergePdf('some_sha1_job_id', 185150, [
+					'https://api.telegram.org/others/lorem.pdf'
+				])
+			).to.be.rejectedWith('Missing required parameters.');
+		});
+
+		it('should rejects with an Error when getOriginalFileInformationFromURL return null', async () => {
+			const params = [
+				'some_sha1_job_id',
+				185150,
+				[
+					'https://api.telegram.org/others/lorem.pdf',
+					'https://api.telegram.org/others/ipsum.pdf',
+					'https://api.telegram.org/others/dolor.pdf'
+				]
+			];
+
+			const setup = [
+				[
+					null,
+					{ name: 'ipsum', extension: 'pdf', filename: 'ipsum.pdf' },
+					null
+				],
+				[
+					{ name: 'lorem', extension: 'pdf', filename: 'lorem.pdf' },
+					null,
+					{ name: 'dolor', extension: 'pdf', filename: 'dolor.pdf' }
+				]
+			];
+
+			for (const x of setup) {
+				const utilStub = sinon.stub(
+					ILoveApiUtils.default,
+					'getOriginalFileInformationFromURL'
+				);
+
+				utilStub.onCall(0).returns(x[0]);
+				utilStub.onCall(1).returns(x[1]);
+				utilStub.onCall(2).returns(x[2]);
+
+				await expect(ILovePDFController.mergePdf(...params)).to.be.rejectedWith(
+					'Failed to resolve original file details.'
+				);
+
+				expect(utilStub.firstCall.returnValue).to.be.deep.equal(x[0]);
+				expect(utilStub.secondCall.returnValue).to.be.deep.equal(x[1]);
+				expect(utilStub.thirdCall.returnValue).to.be.deep.equal(x[2]);
+
+				utilStub.restore();
+			}
+		});
+
+		it('should call service with correct parameters and return mocked value', async () => {
+			spyServiceStub.resetHistory();
+
+			const params = [
+				'some_sha1_job_id',
+				185150,
+				[
+					'https://api.telegram.org/others/lorem.pdf',
+					'https://api.telegram.org/others/ipsum.pdf',
+					'https://api.telegram.org/others/dolor.pdf'
+				]
+			];
+
+			const result = await ILovePDFController.mergePdf(...params);
+
+			expect(spyServiceStub.calledOnceWith(...params)).to.be.true;
+			expect(result).to.be.deep.equal(mockServiceValue);
+		});
+	});
 });
