fb429dbdce23090892db271a090797643ef48250
Reyhan Naufal Hayyan <65756168+rheyhannh@users.noreply.github.com>
2025-08-26 21:59:03 +0700

    feat(bot): Log incoming document, image and command from users

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,14 @@
 import { Telegraf, Composer } from 'telegraf';
 import { message } from 'telegraf/filters';
 import config from './config/global.js';
+import logger from './utils/logger.js';
+import * as _TTLCache from './config/ttlcache.js';
 import * as _Middleware from './middlewares/bot.js';
 import * as _Utils from './utils/bot.js';
 
 const Middleware = _Middleware.default;
 const Utils = _Utils.default;
+const TTLCache = _TTLCache.default;
 
 /**
  * @typedef {Object} buildTelegramBotReturnType
@@ -93,36 +96,248 @@ export async function buildTelegramBot() {
 
 	bot.command('getSharedCreditStates', Middleware.getSharedCreditStates);
 
-	bot.command('pulsa', async (ctx) => {
-		await ctx.reply(
-			'Pulsa di Filebuds adalah kredit yang bisa kamu gunakan untuk memproses file gambar atau PDF dengan cepat. ' +
-				'Layaknya pulsa HP, kamu bisa isi ulang lewat /isipulsa dan cek sisa pulsa dengan /cekpulsa.' +
-				'\n\nDengan menggunakan pulsa :' +
-				'\n- Aksesmu pakai fast track sehingga lebih cepat dibanding pengguna gratis⚡' +
-				'\n- Tidak ada batasan jumlah file yang bisa diproses, selama pulsamu masih tersedia🚀'
-		);
-	});
+	bot.command(
+		'mergepdf',
+		Composer.compose([
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const userId = ctx.chat.id;
 
-	bot.command('isipulsa', async (ctx) => {
-		await ctx.reply(
-			'Saat ini fitur pulsa belum tersedia. ' +
-				'Tapi tenang, kamu masih bisa pakai semua fitur Filebuds lewat akses gratis dari /subsidi setiap harinya. ' +
-				'Yuk manfaatkan sebelum kuotanya habis🤑'
-		);
-	});
+					logger.info(`Received "mergepdf" command from ${user} [${userId}]`);
+				} catch (error) {
+					logger.error(
+						error,
+						'Error logging event while receiving "mergepdf" command'
+					);
+				} finally {
+					await next();
+				}
+			},
+			async (ctx) => {
+				const replyMsg = await ctx.reply(
+					'Silahkan kirim file PDF yang ingin diproses dengan membalas pesan ini. ' +
+						'Pastikan setiap file berformat PDF dan ukurannya tidak lebih dari 5MB. ' +
+						'File yang sudah dikirim akan ditampilkan dalam pesan ini secara berurutan — pastikan urutannya sudah benar.' +
+						`\n\n🚧 Kamu dapat mengirim file sampai 1 hari kedepan.`
+				);
 
-	bot.command('cekpulsa', async (ctx) => {
-		await ctx.reply(
-			'Saat ini fitur pulsa belum tersedia. ' +
-				'Tapi tenang, kamu masih bisa pakai semua fitur Filebuds lewat akses gratis dari /subsidi setiap harinya. ' +
-				'Yuk manfaatkan sebelum kuotanya habis🤑'
-		);
-	});
+				/**
+				 * Unique identifier of cached message, see {@link _TTLCache.CachedMessageId}
+				 */
+				const mid = `${ctx.chat.id}${replyMsg.message_id}`;
+				TTLCache.userMessageUploadCache.set(mid, {
+					userId: ctx.chat.id,
+					messageId: replyMsg.message_id,
+					tool: 'merge',
+					fileType: 'pdf',
+					files: []
+				});
+			}
+		])
+	);
+
+	bot.command(
+		'bulkimage',
+		Composer.compose([
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const userId = ctx.chat.id;
+
+					logger.info(`Received "bulkimage" command from ${user} [${userId}]`);
+				} catch (error) {
+					logger.error(
+						error,
+						'Error logging event while receiving "bulkimage" command'
+					);
+				} finally {
+					await next();
+				}
+			},
+			async (ctx) => {
+				const replyMsg = await ctx.reply(
+					'Silahkan kirim file yang ingin diproses dengan membalas pesan ini. ' +
+						'Pastikan setiap file berformat (.jpg, .png, .jpeg) dan ukurannya tidak lebih dari 5MB. ' +
+						'File yang sudah dikirim akan ditampilkan dalam pesan ini secara berurutan — pastikan urutannya sudah benar.' +
+						`\n\n🚧 Kamu dapat mengirim file sampai 1 hari kedepan.`
+				);
+
+				/**
+				 * Unique identifier of cached message, see {@link _TTLCache.CachedMessageId}
+				 */
+				const mid = `${ctx.chat.id}${replyMsg.message_id}`;
+				TTLCache.userMessageUploadCache.set(mid, {
+					userId: ctx.chat.id,
+					messageId: replyMsg.message_id,
+					tool: 'merge',
+					fileType: 'image',
+					files: []
+				});
+			}
+		])
+	);
+
+	bot.command(
+		'subsidi',
+		Composer.compose([
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const userId = ctx.chat.id;
+
+					logger.info(`Received "subsidi" command from ${user} [${userId}]`);
+				} catch (error) {
+					logger.error(
+						error,
+						'Error logging event while receiving "subsidi" command'
+					);
+				} finally {
+					await next();
+				}
+			},
+			async (ctx) => {
+				await ctx.reply(
+					'Setiap hari, Filebuds menyediakan akses gratis untuk semua pengguna lewat subsidi.' +
+						'\n- Jumlahnya terbatas dan berlaku siapa cepat, dia dapat⏳' +
+						'\n- Kalau subsidi hari ini sudah habis, kamu bisa pakai /pulsa untuk akses fast track⚡'
+				);
+			}
+		])
+	);
+
+	bot.command(
+		'pulsa',
+		Composer.compose([
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const userId = ctx.chat.id;
+
+					logger.info(`Received "pulsa" command from ${user} [${userId}]`);
+				} catch (error) {
+					logger.error(
+						error,
+						'Error logging event while receiving "pulsa" command'
+					);
+				} finally {
+					await next();
+				}
+			},
+			async (ctx) => {
+				await ctx.reply(
+					'Pulsa di Filebuds adalah kredit yang bisa kamu gunakan untuk memproses file gambar atau PDF dengan cepat. ' +
+						'Layaknya pulsa HP, kamu bisa isi ulang lewat /isipulsa dan cek sisa pulsa dengan /cekpulsa.' +
+						'\n\nDengan menggunakan pulsa :' +
+						'\n- Aksesmu pakai fast track sehingga lebih cepat dibanding pengguna gratis⚡' +
+						'\n- Tidak ada batasan jumlah file yang bisa diproses, selama pulsamu masih tersedia🚀'
+				);
+			}
+		])
+	);
+
+	bot.command(
+		'isipulsa',
+		Composer.compose([
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const userId = ctx.chat.id;
+
+					logger.info(`Received "isipulsa" command from ${user} [${userId}]`);
+				} catch (error) {
+					logger.error(
+						error,
+						'Error logging event while receiving "isipulsa" command'
+					);
+				} finally {
+					await next();
+				}
+			},
+			async (ctx) => {
+				await ctx.reply(
+					'Saat ini fitur pulsa belum tersedia. ' +
+						'Tapi tenang, kamu masih bisa pakai semua fitur Filebuds lewat akses gratis dari /subsidi setiap harinya. ' +
+						'Yuk manfaatkan sebelum kuotanya habis🤑'
+				);
+			}
+		])
+	);
+
+	bot.command(
+		'cekpulsa',
+		Composer.compose([
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const userId = ctx.chat.id;
+
+					logger.info(`Received "cekpulsa" command from ${user} [${userId}]`);
+				} catch (error) {
+					logger.error(
+						error,
+						'Error logging event while receiving "cekpulsa" command'
+					);
+				} finally {
+					await next();
+				}
+			},
+			async (ctx) => {
+				await ctx.reply(
+					'Saat ini fitur pulsa belum tersedia. ' +
+						'Tapi tenang, kamu masih bisa pakai semua fitur Filebuds lewat akses gratis dari /subsidi setiap harinya. ' +
+						'Yuk manfaatkan sebelum kuotanya habis🤑'
+				);
+			}
+		])
+	);
 
 	bot.on(
 		'callback_query',
 		Composer.compose([
 			Middleware.initCallbackQueryState,
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const {
+						type,
+						jobId,
+						tool,
+						toolPrice,
+						tg_user_id: userId
+					} = /** @type {_Middleware.CallbackQueryStateProps} */ (
+						ctx.state || {}
+					);
+
+					switch (type) {
+						case 'job_track':
+							logger.info(
+								`Received job_track [${jobId}] from ${user} [${userId}]`
+							);
+							break;
+
+						case 'task_init':
+							logger.info(
+								`Received task_init (${tool}) from ${user} [${userId}], costing ${toolPrice} credits`
+							);
+							break;
+					}
+				} catch (error) {
+					logger.error(
+						error,
+						'Error logging event while receiving callback queries'
+					);
+				} finally {
+					await next();
+				}
+			},
 			Middleware.checkUsersCreditCallbackQueryHandler,
 			Middleware.checkSharedCreditCallbackQueryHandler,
 			Middleware.checkCallbackQueryLimit,
@@ -135,6 +350,31 @@ export async function buildTelegramBot() {
 	bot.on(
 		message('photo'),
 		Composer.compose([
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const userId = ctx.chat.id;
+					const replyMsgId = ctx.message?.reply_to_message?.message_id;
+					const photos = ctx.message.photo;
+					const { file_unique_id } = photos[photos.length - 1];
+
+					if (replyMsgId) {
+						const mid = `${userId}${replyMsgId}`;
+						logger.info(
+							`Received photo [${file_unique_id}] from ${user} [${userId}] to update cached message [${mid}]`
+						);
+					} else {
+						logger.info(
+							`Received photo [${file_unique_id}] from ${user} [${userId}] to select tools`
+						);
+					}
+				} catch (error) {
+					logger.error(error, 'Error logging event while receiving photos');
+				} finally {
+					await next();
+				}
+			},
 			Middleware.validatePhotoMessageMedia,
 			Middleware.handlePhotoMessage
 		])
@@ -143,6 +383,30 @@ export async function buildTelegramBot() {
 	bot.on(
 		message('document'),
 		Composer.compose([
+			// TODO: Refactor.
+			async (ctx, next) => {
+				try {
+					const user = Utils.getUserFromContext(ctx);
+					const userId = ctx.chat.id;
+					const replyMsgId = ctx.message?.reply_to_message?.message_id;
+					const { file_unique_id } = ctx.message.document;
+
+					if (replyMsgId) {
+						const mid = `${userId}${replyMsgId}`;
+						logger.info(
+							`Received document [${file_unique_id}] from ${user} [${userId}] to update cached message [${mid}]`
+						);
+					} else {
+						logger.info(
+							`Received document [${file_unique_id}] from ${user} [${userId}] to select tools`
+						);
+					}
+				} catch (error) {
+					logger.error(error, 'Error logging event while receiving documents');
+				} finally {
+					await next();
+				}
+			},
 			Middleware.validateDocumentMessageMedia,
 			Middleware.handleDocumentMessage
 		])
